ec58adef0143d9a8507770deaa324b16ebac0a40
yuyashirane <165985156+yuyashirane@users.noreply.github.com>
2024-05-06 08:09:22 +0900

    課題の提出

---
+++ kadai_004/event.js
@@ -0,0 +1,10 @@
+$(function () {
+});
+
+$(window).on('load', function () {
+    console.log('loadイベントが発生しました')
+});
+
+$(window).on('scroll', function () {
+    console.log('scrollイベントが発生しました')
+});
